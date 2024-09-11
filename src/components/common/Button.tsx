1bfa42430f491834d55a3cfdb96175e8641a5208
ywj3493 <ywj3493@naver.com>
2024-09-11 21:10:07 +0900

    💄: 버튼 디자인 개선

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
-      className={`bg-blue-300 rounded text-sm text-white px-5 py-2 disabled:bg-gray-300 ${className}`}
+      className={`bg-blue-300 rounded text-sm text-white font-extrabold px-5 py-2 disabled:bg-gray-300 ${className}`}
       {...props}
     ></button>
   );
